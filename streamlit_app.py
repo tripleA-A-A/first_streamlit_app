81da94979b1cc670504912757703e767c085374a
tripleA-A-A <abdir98@googlemail.com>
2022-12-15 13:29:16 +0000

    bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def get_fruit_load_list():
 
 if streamlit.button("Get Fruit Load List"):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_row = get_fruit_load_list
+    my_data_row = get_fruit_load_list()
     streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
