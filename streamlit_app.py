b5a4170964ae888878876ed11ba2280c2a51c349
tripleA-A-A <abdir98@googlemail.com>
2022-12-15 12:56:08 +0000

    Errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import requests
+import snowflake.connector
 
 streamlit.title("Breakfast Favorites")
 streamlit.header("Breakfast Menu")
@@ -20,7 +20,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input("What fruit would you like information about?")
+fruit_choice = streamlit.text_input("What fruit would you like information about?","kiwi")
 streamlit.write("The user entered",fruit_choice)
 
 import requests
