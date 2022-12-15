92b2763657d062751fdf3f2d0de8edefc30bd111
tripleA-A-A <abdir98@googlemail.com>
2022-12-15 13:10:57 +0000

    SOMETHING

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("Breakfast Favorites")
 streamlit.header("Breakfast Menu")
@@ -23,12 +25,13 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input("What fruit would you like information about?","kiwi")
 streamlit.write("The user entered",fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalised)
 
+streamlit.stop
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
