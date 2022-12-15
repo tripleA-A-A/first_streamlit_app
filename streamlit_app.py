529da7d361aee53312443462693642692d437e54
tripleA-A-A <abdir98@googlemail.com>
2022-12-15 13:01:39 +0000

    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.dataframe(fruityvice_normalised)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fuit_load_list")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The list contains")
 streamlit.text(my_data_row)
