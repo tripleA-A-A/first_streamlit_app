ac737a972617f51534bd3337c3c15645b9723ccb
tripleA-A-A <abdir98@googlemail.com>
2022-12-15 13:08:14 +0000

    from streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,4 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input("What fruit would you like to add?")
 streamlit.text("Thanks for adding " + add_my_fruit)
+my_cur.execute("INSERT INTO fruit_load_list values ('from streamlit')")
