811ae7c6a60af6b166acb1da30053516a8e13e91
tripleA-A-A <abdir98@googlemail.com>
2022-12-15 13:17:13 +0000

    try except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,14 +22,18 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input("What fruit would you like information about?","kiwi")
+try:
+    fruit_choice = streamlit.text_input("What fruit would you like information about?")
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalised)
+except URLError as e:
+    streamlit.error()
+
 streamlit.write("The user entered",fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalised)
 
 streamlit.stop
 
